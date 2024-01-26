42c1ae0510017818b391685ac1b359ef97ccfb77
Yusup04Orazov <152412703+Yusup04Orazov@users.noreply.github.com>
2024-01-25 22:34:36 -0600

    Update TransformerTextGeneration.py

--- TransformerModel copy/TransformerTextGeneration.py
+++ TransformerModel copy/TransformerTextGeneration.py
@@ -1,14 +1,3 @@
-# from transformers import pipeline
-
-# generator = pipeline("text-generation", model = "distilgpt2")
-
-# res = generator(
-#     "in this course, we will teach you how to", 
-#     max_length=30,
-#     num_return_sequences=2,
-# )
-# print(res)
-
 from transformers import pipeline
 import textwrap
 
